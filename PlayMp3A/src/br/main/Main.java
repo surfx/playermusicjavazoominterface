439c7de0de60f29278b7e7b84e9b07d0b5dbb9aa
surfx <ripador0@hotmail.com>
2020-08-05 23:46:47 -0300

    criado .gitignore

--- PlayMp3A/src/br/main/Main.java
+++ PlayMp3A/src/br/main/Main.java
@@ -5,9 +5,7 @@ import br.main.player.visual.JPlayerMp3;
 /**
  * https://introcs.cs.princeton.edu/java/faq/mp3/mp3.html<br />
  * http://www.javazoom.net/javalayer/javalayer.html<br />
- * https://introcs.cs.princeton.edu/java/faq/mp3/MP3.java.html<br />
- * https://stackoverflow.com/questions/16882354/how-to-play-pause-a-mp3-file-using-the-javazoom-jlayer-library<br
- * />
+ * https://stackoverflow.com/questions/16882354/how-to-play-pause-a-mp3-file-using-the-javazoom-jlayer-library<br />
  */
 public class Main {
 
